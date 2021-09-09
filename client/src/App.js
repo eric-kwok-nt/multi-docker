e2bb62c4fa2699b7c93d01180cd20fe3e665a75e
Eric_kwok <kwokngaitung777@gmail.com>
2021-09-09 17:17:21 +0800

    changed client App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,15 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
+          <h1 className="App-title">Fib Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
